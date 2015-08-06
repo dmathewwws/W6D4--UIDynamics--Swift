ca9db330699d33be03140504d28e392ef3d4c490
Daniel Mathews <dannyl.mathews@gmail.com>
2015-08-06 06:59:35 -0700

    Initial Commit

---
+++ W6D4- UIDynamics- Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  W6D4- UIDynamics- Swift
+//
+//  Created by Daniel Mathews on 2015-08-06.
+//  Copyright © 2015 Daniel Mathews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
